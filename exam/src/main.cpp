bec71fdde3a2e513f18490ee2b19fc947f042468
Jacob Wanzenried <62063187+wanzenried@users.noreply.github.com>
2024-11-26 18:29:54 +0100

    install library + identify button pins

--- exam/src/main.cpp
+++ exam/src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+const uint8_t lButton = 7;
+const uint8_t mButton = 9;
+const uint8_t rButton = 11;
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("hello world");
+  pinMode(lButton, INPUT);
+  pinMode(mButton, INPUT);
+  pinMode(rButton, INPUT);
 }
 
 void loop() {
